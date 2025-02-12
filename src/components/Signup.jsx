0355392a3c2adf06feb8998e73c1404e876b7861
Francisco Berroeta <fsberroeta@gmail.com>
2025-02-12 16:21:31 -0300

    feat: add password confirmation validation to Signup component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 export default function Signup() {
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -6,6 +9,11 @@ export default function Signup() {
     const acquisitionChannel = fd.getAll('acquisition');
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChannel;
+
+    if (data.password !== data['confirm-password']) {
+      setPasswordsAreNotEqual(true);
+      return;
+    }
 
     console.log(data);
   }
@@ -40,6 +48,9 @@ export default function Signup() {
             name="confirm-password"
             required
           />
+          <div className="control-error">
+            {passwordsAreNotEqual && <p>Passwords must match.</p>}
+          </div>
         </div>
       </div>
 
