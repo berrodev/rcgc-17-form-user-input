145358e81b982058f2f418379b144d5cf4df1225
Francisco Berroeta <fsberroeta@gmail.com>
2025-02-12 16:42:31 -0300

    feat: replace Signup component with Login component and refactor Input usage for better error handling

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,18 +1,9 @@
-export function Input() {
+export function Input({ label, id, error, ...props }) {
   return (
     <div className="control no-margin">
-      <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-        onBlur={() => handleInputBlur('email')}
-        onChange={(event) => handleInputChange('email', event.target.value)}
-        value={enteredValues.email}
-      />
-      <div className="control-error">
-        {emailIsInvalid && <p>Please enter a valid email</p>}
-      </div>
+      <label htmlFor={id}>{label}</label>
+      <input id={id} {...props} />
+      <div className="control-error">{error && <p>{error}</p>}</div>
     </div>
   );
 }
