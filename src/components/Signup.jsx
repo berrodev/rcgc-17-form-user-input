c4f6ba5a746cf53a139c7b74aae383277f57904a
Francisco Berroeta <fsberroeta@gmail.com>
2025-02-12 14:15:34 -0300

    feat: implement form submission handling in Signup component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,16 @@
 export default function Signup() {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const enteredEmail = fd.get('email');
+    const enteredPassword = fd.get('password');
+
+    console.log(enteredEmail, enteredPassword);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
