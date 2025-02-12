e7d1a1ab3e31b1e55a595bc96d6a7dca1687641a
Francisco Berroeta <fsberroeta@gmail.com>
2025-02-12 13:38:00 -0300

    refactor: enhance Login component to manage form state and input changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+
 export default function Login() {
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log('Submitted');
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: '',
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(enteredValues);
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+  }
+  function handleEmailChange(event) {
+    setEnteredEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setEnteredPassword(event.target.value);
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -10,12 +33,26 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange('email', event.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange('password', event.target.value)
+            }
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
