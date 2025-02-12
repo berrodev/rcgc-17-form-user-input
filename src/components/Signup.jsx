95416127ac922fcca607840c32ad3cd483a4b04b
Francisco Berroeta <fsberroeta@gmail.com>
2025-02-12 14:20:32 -0300

    feat: capture acquisition channel in Signup form submission

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,7 +3,9 @@ export default function Signup() {
     event.preventDefault();
 
     const fd = new FormData(event.target);
+    const acquisitionChannel = fd.getAll('acquisition');
     const data = Object.fromEntries(fd.entries());
+    data.acquisition = acquisitionChannel;
 
     console.log(data);
   }
